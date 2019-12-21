4464efcdf9faf67b13d8c95f517d8e9c005637d1
Ramzy Badawy <ramzy.badawy@gmail.com>
2019-12-21 18:07:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
 numbers = [1,2,3]
 counter = 0
-while each numbers [counter] do
+while numbers [counter] do
   puts numbers [counter]
-  counter ** numbers
+  counter ** each numbers
   end
 end
